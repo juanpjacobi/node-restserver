06e4782f70540b2db9ea9a53e0cfa850f972feea
Juan Pablo <juanpjacobi@gmail.com>
2018-11-25 16:12:54 -0300

    RestServer - USUARIO

--- server/config/config.js
+++ server/config/config.js
@@ -13,6 +13,24 @@ process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
 
 
 // ============================
+//  Vencimiento token
+// ============================
+
+//60 segundos
+//60 minutos
+// 24 horas
+// 30 dias
+
+process.env.CADUCIDAD_TOKEN = 60 * 60 * 24 * 30;
+
+// ============================
+//  SEED de autenticacion
+// ============================
+
+process.env.SEED = process.env.SEED || 'este es el seed de desarrollo'
+
+
+// ============================
 //  Base de datos
 // ============================
 let urlDB;
